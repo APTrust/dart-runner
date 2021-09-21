50dc7edfcdc9728655635449240c401220eaa427
A. Diamond <diamond@sheprador.com>
2021-09-21 15:14:15 -0400

    Doc strings and checksum validation cleanup

--- validator/file_map.go
+++ validator/file_map.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 )
 
+// FileType describes the type of file in a bag.
 type FileType int
 
 const (
@@ -13,6 +14,7 @@ const (
 	FileTypeTagManifest
 )
 
+// prefixFor is used in formatting errors messages.
 var prefixFor = []string{
 	"",
 	"",
@@ -20,13 +22,23 @@ var prefixFor = []string{
 	"tag-",
 }
 
+// MaxErrors is the maximum number of errors the validator will
+// collect before quitting and returning the error list. We don't
+// quit at the first error because when developing a bagging
+// process, multiple errors are common and we don't want to make
+// depositors have to rebag constantly just to see the next error.
+// We also try to be very specific with error messages so depositors
+// know exactly what to fix.
 const MaxErrors = 30
 
+// FileMap contains a map of FileRecord objects and some methods to
+// help validate those records.
 type FileMap struct {
 	Type  FileType
 	Files map[string]*FileRecord
 }
 
+// NewFileMap returns a pointer to a new FileMap object.
 func NewFileMap(fileType FileType) *FileMap {
 	return &FileMap{
 		Type:  fileType,
@@ -34,10 +46,17 @@ func NewFileMap(fileType FileType) *FileMap {
 	}
 }
 
+// ValidateChecksums validates all checksums for all files in this
+// FileMap. Param algs is a list of algorithms for manifests found
+// in the bag.
 func (fm *FileMap) ValidateChecksums(algs []DigestAlgorithm) []error {
+
+	// TODO: If fm.Type is TagFile, it doesn't have to be included
+	// in the tag manifests, so we can do looser validation here.
+
 	errors := make([]error, 0)
 	for name, file := range fm.Files {
-		err := file.Validate(algs)
+		err := file.Validate(fm.Type, algs)
 		if err != nil {
 			errors = append(errors, fmt.Errorf("File %s: %s", name, err.Error()))
 			if len(errors) >= MaxErrors {
--- validator/file_record.go
+++ validator/file_record.go
@@ -4,65 +4,69 @@ import (
 	"fmt"
 )
 
+// ErrFileMissingFromBag indicates that a file present in a payload
+// manifest is not present in the bag's data directory.
 var ErrFileMissingFromBag = fmt.Errorf("file is missing from bag")
 
+// FileRecord contains a collection of checksums for a single file.
 type FileRecord struct {
 	Checksums []*Checksum
 }
 
+// NewFileRecord returns a pointer to a new FileRecord object.
 func NewFileRecord() *FileRecord {
 	return &FileRecord{
 		Checksums: make([]*Checksum, 0),
 	}
 }
 
-func (fr *FileRecord) AddChecksum(checksum *Checksum) {
-	fr.Checksums = append(fr.Checksums, checksum)
+// AddChecksum adds a checksum to this FileRecord.
+func (fr *FileRecord) AddChecksum(source ChecksumSource, alg DigestAlgorithm, digest string) {
+	fr.Checksums = append(fr.Checksums, NewChecksum(source, alg, digest))
 }
 
-func (fr *FileRecord) Validate(algs []DigestAlgorithm) error {
-	if !fr.FileExists() {
-		return ErrFileMissingFromBag
+// Validate validates the following about the current file:
+//
+// * file is present in the payload directory
+// * file is listed in the payload manifests matching the
+//   specified algorithms.
+// * the checksums that the validator calculated on the file itself
+//   match the checksums in the manifests.
+// Validate returns true if the checksums we calculated for the
+// file match the checksums in the manifests.
+func (fr *FileRecord) Validate(fileType FileType, algs []DigestAlgorithm) error {
+	srcFile := SourcePayloadFile
+	srcManifest := SourceManifest
+	if fileType == FileTypeTagFile {
+		srcFile = SourceTagFile
+		srcManifest = SourceTagManifest
 	}
 	for _, alg := range algs {
-		if !fr.ManifestEntryExists(alg) {
-			return fmt.Errorf("missing %s digest entry", algNames[alg])
+		fileChecksum := fr.GetChecksum(alg, srcFile)
+		if fileChecksum == nil {
+			return ErrFileMissingFromBag
+		}
+		manifestChecksum := fr.GetChecksum(alg, srcManifest)
+		if srcManifest == SourceTagManifest && manifestChecksum == nil {
+			continue // tag files don't have to appear in tag manifests
+		}
+		if manifestChecksum == nil {
+			return fmt.Errorf("file is missing from manifest manifest-%s.txt", algNames[alg])
+		}
+		if fileChecksum.Digest != manifestChecksum.Digest {
+			return fmt.Errorf("Digest %s in %s does not match digest %s in %s", manifestChecksum.Digest, manifestChecksum.SourceName(), fileChecksum.Digest, fileChecksum.SourceName())
 		}
 	}
-	_, err := fr.ChecksumsMatch()
-	return err
+	return nil
 }
 
-func (fr *FileRecord) ChecksumsMatch() (bool, error) {
-	digest := ""
-	source := ""
-	for _, c := range fr.Checksums {
-		if digest == "" {
-			digest = c.Digest
-			source = c.SourceName()
-			continue
-		}
-		if c.Digest != digest {
-			return false, fmt.Errorf("Digest %s in %s does not match digest %s in %s", c.Digest, c.SourceName(), digest, source)
+// GetChecksum returns the checksum with the specified algorithm
+// and source.
+func (fr *FileRecord) GetChecksum(alg DigestAlgorithm, source ChecksumSource) *Checksum {
+	for _, cs := range fr.Checksums {
+		if cs.Algorithm == alg && cs.Source == source {
+			return cs
 		}
 	}
-	return true, nil
-}
-
-func (fr *FileRecord) FileExists() bool {
-	for _, c := range fr.Checksums {
-		if c.Source == SourcePayloadFile || c.Source == SourceTagFile {
-			return true
-		}
-	}
-	return false
-}
-
-func (fr *FileRecord) ManifestEntryExists(alg DigestAlgorithm) bool {
-	for _, c := range fr.Checksums {
-		if c.Algorithm == alg {
-			return true
-		}
-	}
-	return false
+	return nil
 }
