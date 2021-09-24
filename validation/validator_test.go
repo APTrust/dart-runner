0b147e359750c75c3b851e4398f4d645a5f52331
A. Diamond <diamond@sheprador.com>
2021-09-24 17:05:18 -0400

    Added some BTR validation tests

--- validation/validator_test.go
+++ validation/validator_test.go
@@ -183,7 +183,6 @@ func TestValidator_BadTags(t *testing.T) {
 	require.Nil(t, err)
 	isValid = v.Validate()
 	assert.False(t, isValid)
-	fmt.Println(v.ErrorString())
 	assert.Equal(t, 3, len(v.Errors))
 	assert.Equal(t, "Required manifest is missing.", v.Errors["md5"])
 	assert.Equal(t, "Required tag is missing.", v.Errors["aptrust-info.txt/Access"])
@@ -212,6 +211,53 @@ func TestValidator_GoodBTRBags(t *testing.T) {
 			assert.Empty(t, v.Errors, message)
 		}
 	}
+}
+
+func TestValidator_BTRBadChecksums(t *testing.T) {
+	v := getValidator(t, "test.edu.btr_bad_checksums.tar", btrProfile)
+	err := v.ScanBag()
+	require.Nil(t, err)
+	isValid := v.Validate()
+	assert.False(t, isValid)
+	assert.Equal(t, 1, len(v.Errors))
+	assert.Equal(t, "Digest 00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000 in manifest-sha512.txt does not match digest b598afc318e6d06f54a162e8e43bbc9cb071fcf0ffb3766b719011d8403d01290d6f2d7a9decc504395501f28f6c452c5a4317ee7bd309d4cd597984227d176d in payload file", v.Errors["data/netutil/listen.go"])
+}
+
+func TestValidator_BTRExtraFile(t *testing.T) {
+	v := getValidator(t, "test.edu.btr_bad_extraneous_file.tar", btrProfile)
+	err := v.ScanBag()
+	require.NotNil(t, err)
+	assert.Equal(t, 1, len(v.Errors))
+	assert.Equal(t, "Payload-Oxum does not match payload", v.Errors["Payload-Oxum"])
+}
+
+func TestValidator_BTRMissingPayloadFile(t *testing.T) {
+	v := getValidator(t, "test.edu.btr_bad_missing_payload_file.tar", btrProfile)
+	err := v.ScanBag()
+	require.NotNil(t, err)
+	assert.Equal(t, 1, len(v.Errors))
+	assert.Equal(t, "Payload-Oxum does not match payload", v.Errors["Payload-Oxum"])
+}
+
+func TestValidator_BTRMissingTags(t *testing.T) {
+	v := getValidator(t, "test.edu.btr_bad_missing_required_tags.tar", btrProfile)
+	err := v.ScanBag()
+	require.Nil(t, err)
+	isValid := v.Validate()
+	assert.False(t, isValid)
+	assert.Equal(t, 3, len(v.Errors))
+
+	// Note that BTR requires Payload-Oxum while APTrust does not.
+	assert.Equal(t, "Required tag is missing.", v.Errors["bag-info.txt/Bagging-Date"])
+	assert.Equal(t, "Required tag is missing.", v.Errors["bag-info.txt/Payload-Oxum"])
+	assert.Equal(t, "Required tag is missing.", v.Errors["bag-info.txt/Source-Organization"])
+}
+
+func TestValidator_IllegalControlCharacter(t *testing.T) {
+	// Review the BagIt spec on this. It's actually pretty
+	// lax and puts the burden on the bagger to ensure
+	// good practices. Sections 5 and 6 give some guidance:
+	// https://datatracker.ietf.org/doc/html/rfc8493
 }
 
 // TODO:
