dc147e278263aabc0425f06db8fb866ee3cf550c
A. Diamond <diamond@sheprador.com>
2021-10-01 16:22:31 -0400

    Refactored bagger tests using both APTrust and BTR profiles.

--- bagit/bagger_test.go
+++ bagit/bagger_test.go
@@ -1,7 +1,6 @@
 package bagit_test
 
 import (
-	"fmt"
 	"os"
 	"path"
 	"strings"
@@ -13,6 +12,12 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
+const (
+	APTProfile   = "aptrust-v2.2.json"
+	BTRProfile   = "btr-v1.0.json"
+	EmptyProfile = "empty_profile.json"
+)
+
 func getBagger(t *testing.T, bagName, profileName string, files []*util.ExtendedFileInfo) *bagit.Bagger {
 	outputPath := path.Join(os.TempDir(), bagName)
 	profile, err := loadProfile(profileName)
@@ -22,28 +27,33 @@ func getBagger(t *testing.T, bagName, profileName string, files []*util.Extended
 	return bagger
 }
 
-func TestBaggerRun(t *testing.T) {
+func TestBaggerRun_APTrust(t *testing.T) {
+	testBaggerRun(t, "apt_bag.tar", APTProfile)
+}
+
+func TestBaggerRun_BTR(t *testing.T) {
+	testBaggerRun(t, "btr_bag.tar", BTRProfile)
+}
+
+func testBaggerRun(t *testing.T, bagName, profileName string) {
 	files, err := util.RecursiveFileList(util.PathToTestData())
 	require.Nil(t, err)
-	bagger := getBagger(t, "bag01.tar", "aptrust-v2.2.json", files)
-	// defer os.Remove(bagger.OutputPath)
+	bagger := getBagger(t, bagName, profileName, files)
+	defer os.Remove(bagger.OutputPath)
 
-	bagger.Profile.SetTagValue("bag-info.txt", "Source-Organization", "University of Virginia")
-	bagger.Profile.SetTagValue("bag-info.txt", "Bag-Count", "1 of 1")
-	bagger.Profile.SetTagValue("bag-info.txt", "Internal-Sender-Description", "My stuff")
-	bagger.Profile.SetTagValue("bag-info.txt", "Internal-Sender-Identifier", "my-identifier")
+	setBagInfoTags(bagger.Profile)
 
-	bagger.Profile.SetTagValue("aptrust-info.txt", "Title", "Test Bag #0001")
-	bagger.Profile.SetTagValue("aptrust-info.txt", "Description", "Eloquence and elocution")
-	bagger.Profile.SetTagValue("aptrust-info.txt", "Access", "Consortia")
-	bagger.Profile.SetTagValue("aptrust-info.txt", "Storage-Option", "Glacier-Deep-OH")
+	if profileName == APTProfile {
+		setAptInfoTags(bagger.Profile)
+	}
 
+	// Create the bag and ensure no errors
 	ok := bagger.Run()
 	assert.True(t, ok)
 	assert.Empty(t, bagger.Errors)
-	fmt.Println(bagger.OutputPath)
 
-	profile, err := loadProfile("aptrust-v2.2.json")
+	// Validate the bag
+	profile, err := loadProfile(profileName)
 	require.Nil(t, err)
 	require.NotNil(t, profile)
 	validator, err := bagit.NewValidator(bagger.OutputPath, profile)
@@ -55,11 +65,32 @@ func TestBaggerRun(t *testing.T) {
 	assert.True(t, validator.Validate())
 	assert.Empty(t, validator.Errors)
 
+	// In addition to being valid, make sure the payload
+	// has everything we expect.
 	xFileInfoList, err := util.RecursiveFileList(util.PathToTestData())
 	require.Nil(t, err)
 	assertAllPayloadFilesPresent(t, xFileInfoList, validator.PayloadFiles.Files)
 }
 
+// Set tags for bag-info.txt in the profile before we create the bag.
+func setBagInfoTags(profile *bagit.Profile) {
+	profile.SetTagValue("bag-info.txt", "Source-Organization", "University of Virginia")
+	profile.SetTagValue("bag-info.txt", "Bag-Count", "1 of 1")
+	profile.SetTagValue("bag-info.txt", "Internal-Sender-Description", "My stuff")
+	profile.SetTagValue("bag-info.txt", "Internal-Sender-Identifier", "my-identifier")
+}
+
+// Set tags for aptrust-info.txt in the profile before we create the bag.
+func setAptInfoTags(profile *bagit.Profile) {
+	profile.SetTagValue("aptrust-info.txt", "Title", "Test Bag #0001")
+	profile.SetTagValue("aptrust-info.txt", "Description", "Eloquence and elocution")
+	profile.SetTagValue("aptrust-info.txt", "Access", "Consortia")
+	profile.SetTagValue("aptrust-info.txt", "Storage-Option", "Glacier-Deep-OH")
+}
+
+// Make sure that all items expected to be in the payload are actually there.
+// Just because manifest and payload match doesn't mean that payload contains
+// everything we intended to bag.
 func assertAllPayloadFilesPresent(t *testing.T, expected []*util.ExtendedFileInfo, actual map[string]*bagit.FileRecord) {
 	for _, xFileInfo := range expected {
 		if xFileInfo.IsDir() {
