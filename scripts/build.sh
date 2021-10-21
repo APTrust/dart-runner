e55a2d9ece4d700f1237ae137505e3af849c8ac3
A. Diamond <diamond@sheprador.com>
2021-10-21 09:54:02 -0400

    Added Windows build support

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-
-if [ -z $(ls scripts/build.sh 2> /dev/null) ]
-then
+if [ -z $(ls scripts/build.sh 2> /dev/null) ]; then
     echo "Run this script from the project root directory"
     exit
 fi
@@ -12,11 +10,20 @@ TAG=$(git describe --tags 2> /dev/null)
 DATE=$(date +%Y-%m-%d)
 OS=$(uname -ms)
 
+BUILD_TAGS=""
+
+# uname returns MINGW64_NT-10.0 on Windows 10 Cygwin
+# and MSYS_NT-10.0 on Windows 10 cmd.
+if [[ "$OS" == *"_NT-"* ]]; then
+	OS="Windows $(uname -m)"
+	BUILD_TAGS="-tags windows"
+fi
+
 TAG="${TAG:=Beta 0.1}"
 VERSION="DART Runner $TAG for $OS (Build $COMMIT $DATE)"
 
 mkdir -p dist
-go build -o dist/dart-runner -ldflags "-X 'main.Version=$VERSION'"
+go build -o dist/dart-runner -ldflags "-X 'main.Version=$VERSION'" $BUILD_TAGS
 
 echo "Executable is in dist/dart-runner"
 dist/dart-runner --version
