c251814e228fb6bcb7e7f6f03c190bd2f95a1d48
A. Diamond <diamond@sheprador.com>
2021-12-02 12:50:36 -0500

    Still trying to fix pipe issue on Travis

--- util/command.go
+++ util/command.go
@@ -9,6 +9,9 @@ const STDIN_ERROR = -10000
 
 // ExecCommand executes a command and returns the output of STDOUT and STDERR
 // as well as the exit code.
+//
+// os/exec has some problems when piping stdin. These show up on Travis/Linux,
+// but not Mac. See https://github.com/golang/go/issues/9307
 func ExecCommand(command string, args []string, stdinData []byte) (stdout, stderr []byte, exitCode int) {
 	cmd := exec.Command(command, args...)
 	var outBuffer, errBuffer bytes.Buffer
@@ -27,7 +30,8 @@ func ExecCommand(command string, args []string, stdinData []byte) (stdout, stder
 		stdin.Close()
 	}
 
-	cmd.Run()
+	cmd.Start()
+	cmd.Wait()
 	exitCode = cmd.ProcessState.ExitCode()
 	return outBuffer.Bytes(), errBuffer.Bytes(), exitCode
 }
