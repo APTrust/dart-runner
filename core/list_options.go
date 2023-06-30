510f2649311dfa654eebb00a5488524f27cc6f0e
A. Diamond <diamond@sheprador.com>
2023-06-30 12:48:17 -0400

    Added ChoiceList functions

--- core/list_options.go
+++ core/list_options.go
@@ -7,3 +7,29 @@ func YesNoChoices(trueOrFalse bool) []Choice {
 		{Label: "No", Value: "false", Selected: !trueOrFalse},
 	}
 }
+
+func MakeChoiceList(options []string, selectedValue string) []Choice {
+	choices := make([]Choice, len(options)+1)
+	choices[0] = Choice{Label: "", Value: ""}
+	for i, item := range options {
+		selected := item == selectedValue
+		choices[i+1] = Choice{Label: item, Value: item, Selected: selected}
+	}
+	return choices
+}
+
+func MakeMultiChoiceList(options []string, selectedValues []string) []Choice {
+	choices := make([]Choice, len(options)+1)
+	choices[0] = Choice{Label: "", Value: ""}
+	for i, item := range options {
+		selected := false
+		for _, selectedValue := range selectedValues {
+			if item == selectedValue {
+				selected = true
+				break
+			}
+		}
+		choices[i+1] = Choice{Label: item, Value: item, Selected: selected}
+	}
+	return choices
+}
--- core/list_options_test.go
+++ core/list_options_test.go
@@ -3,8 +3,10 @@ package core_test
 import (
 	"testing"
 
+	"github.com/APTrust/dart-runner/constants"
 	"github.com/APTrust/dart-runner/core"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func TestYesNoChoices(t *testing.T) {
@@ -20,4 +22,39 @@ func TestYesNoChoices(t *testing.T) {
 	choices = core.YesNoChoices(false)
 	assert.False(t, choices[1].Selected)
 	assert.True(t, choices[2].Selected)
+}
+
+func TestMakeChoiceList(t *testing.T) {
+	choices := core.MakeChoiceList(constants.PreferredAlgsInOrder, constants.AlgSha256)
+	require.Equal(t, len(constants.PreferredAlgsInOrder)+1, len(choices))
+	assert.Equal(t, "", choices[0].Label)
+	assert.Equal(t, "", choices[0].Value)
+	for i, alg := range constants.PreferredAlgsInOrder {
+		choice := choices[i+1]
+		assert.Equal(t, constants.PreferredAlgsInOrder[i], choice.Label)
+		assert.Equal(t, constants.PreferredAlgsInOrder[i], choice.Value)
+		if alg == constants.AlgSha256 {
+			assert.True(t, choice.Selected)
+		} else {
+			assert.False(t, choice.Selected)
+		}
+	}
+}
+
+func TestMakeMultiChoiceList(t *testing.T) {
+	selected := []string{constants.AlgSha256, constants.AlgSha1}
+	choices := core.MakeMultiChoiceList(constants.PreferredAlgsInOrder, selected)
+	require.Equal(t, len(constants.PreferredAlgsInOrder)+1, len(choices))
+	assert.Equal(t, "", choices[0].Label)
+	assert.Equal(t, "", choices[0].Value)
+	for i, alg := range constants.PreferredAlgsInOrder {
+		choice := choices[i+1]
+		assert.Equal(t, constants.PreferredAlgsInOrder[i], choice.Label)
+		assert.Equal(t, constants.PreferredAlgsInOrder[i], choice.Value)
+		if alg == constants.AlgSha256 || alg == constants.AlgSha1 {
+			assert.True(t, choice.Selected)
+		} else {
+			assert.False(t, choice.Selected)
+		}
+	}
 }
