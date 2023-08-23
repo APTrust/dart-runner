c293c680e5fadfe2db35df34fc1d2d77282e7ba8
A. Diamond <diamond@sheprador.com>
2023-08-23 13:59:42 -0400

    Finished tests for SFTPUpload

--- core/sftp_client_test.go
+++ core/sftp_client_test.go
@@ -2,6 +2,7 @@ package core_test
 
 import (
 	"path"
+	"sync"
 	"testing"
 
 	"github.com/APTrust/dart-runner/constants"
@@ -33,7 +34,8 @@ func TestSftpUploadWithPassword(t *testing.T) {
 	require.Nil(t, err)
 	require.NotNil(t, sftpClient)
 
-	testUpload(t, sftpClient, ss)
+	testUploadWithoutProgress(t, sftpClient, ss)
+	testUploadWithProgress(t, sftpClient, ss)
 }
 
 func TestSftpUploadWithSSHKey(t *testing.T) {
@@ -50,14 +52,71 @@ func TestSftpUploadWithSSHKey(t *testing.T) {
 	require.Nil(t, err)
 	require.NotNil(t, sftpClient)
 
-	testUpload(t, sftpClient, ss)
+	testUploadWithoutProgress(t, sftpClient, ss)
+	testUploadWithProgress(t, sftpClient, ss)
 }
 
-func testUpload(t *testing.T, sftpClient *sftp.Client, ss *core.StorageService) {
+func testUploadWithoutProgress(t *testing.T, sftpClient *sftp.Client, ss *core.StorageService) {
 	fileToUpload := path.Join(util.PathToTestData(), "bags", "example.edu.sample_good.tar")
 	bytesCopied, err := core.SFTPUpload(ss, fileToUpload, nil)
 	require.Nil(t, err)
 	assert.Equal(t, int64(23552), bytesCopied)
+}
+
+// This is a tricky test. We want to make sure the SFTP uploader
+// pushes messages into the progress reader's message channel.
+// See comments inline...
+func testUploadWithProgress(t *testing.T, sftpClient *sftp.Client, ss *core.StorageService) {
+	iReadSomething := false
+	messageChannel := make(chan *core.EventMessage)
+
+	// When this function exits, make sure our reader read a message
+	// from the messageChannel. The SFTP uploader is supposed to pump
+	// messages into this channel with info about the upload progress.
+	defer func() {
+		close(messageChannel)
+		assert.True(t, iReadSomething)
+	}()
+
+	// We have to set up a WaitGroup to prevent a data race.
+	// The go routine below is fiddling with the iReadSomething
+	// flag defined above. Unless we wait, our test function could
+	// exit before the go routine below gets to do its work.
+	// That can lead to two problems:
+	// 1. iReadSomething never gets set to true.
+	// 2. The defer function will run too soon, closing the message
+	//    channel, and then the go routine will try to read from a
+	//    closed channel.
+	var wg sync.WaitGroup
+
+	// Define our message channel and a go routine to listen to it.
+	// The go routine will do nothing until later, after we call
+	// core.SFTPUpload below, because until then, there will be no
+	// traffic on the message channel.
+	progress := core.NewS3UploadProgress(int64(23552), messageChannel)
+	go func() {
+		if msg, ok := <-messageChannel; ok {
+			assert.Equal(t, "Sent 23.0 kB of 23.0 kB (100%)", msg.Message)
+			iReadSomething = true
+			wg.Done()
+		}
+	}()
+
+	// Tell our WaitGroup we have a task pending.
+	// Then we'll kick off the upload and wait for WaitGroup
+	// to complete. When the go routine above cathches a message
+	// from core.SFTPUpload, it will call wg.Done(). Then the
+	// wg.Wait() at the end of this function will stop waiting.
+	//
+	// At that point, our defer func above will fire, closing
+	// the message channel and testing to see whether the go
+	// routine actually read a message.
+	wg.Add(1)
+	fileToUpload := path.Join(util.PathToTestData(), "bags", "example.edu.sample_good.tar")
+	bytesCopied, err := core.SFTPUpload(ss, fileToUpload, progress)
+	require.Nil(t, err)
+	assert.Equal(t, int64(23552), bytesCopied)
+	wg.Wait()
 }
 
 func getSftpStorageService() *core.StorageService {
