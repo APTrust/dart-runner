9dd628974f05aae737cb0527dd8a6654246753e1
A. Diamond <diamond@sheprador.com>
2024-01-03 10:26:44 -0500

    More tests for validation job controller

--- server/controllers/validation_job_controller_test.go
+++ server/controllers/validation_job_controller_test.go
@@ -1,9 +1,16 @@
 package controllers_test
 
 import (
+	"fmt"
+	"net/http"
+	"net/url"
+	"path/filepath"
 	"testing"
 
+	"github.com/APTrust/dart-runner/constants"
 	"github.com/APTrust/dart-runner/core"
+	"github.com/APTrust/dart-runner/util"
+	"github.com/stretchr/testify/require"
 )
 
 func TestValidationJobNew(t *testing.T) {
@@ -17,14 +24,38 @@ func TestValidationJobNew(t *testing.T) {
 		"attachDragAndDropEvents",
 	}
 	DoGetTestWithRedirect(t, "/validation_jobs/new", "/validation_jobs/files/", expected)
+
+	valJob := core.ObjList(constants.TypeValidationJob, "obj_name", 1, 0).ValidationJob()
+	require.NotNil(t, valJob)
+
+	testValidationJobShowFiles(t, valJob.ID)
+	testValidationJobAddFile(t, valJob.ID)
 }
 
-func TestValidationJobShowFiles(t *testing.T) {
-
+func testValidationJobShowFiles(t *testing.T, id string) {
+	expected := []string{
+		"jumpMenu",
+		"showHiddenFiles",
+		"file-browser-item",
+		"dropZone",
+		"fileTotals",
+		"attachDragAndDropEvents",
+	}
+	DoSimpleGetTest(t, fmt.Sprintf("/validation_jobs/files/%s", id), expected)
 }
 
-func TestValidationJobAddFile(t *testing.T) {
-
+func testValidationJobAddFile(t *testing.T, id string) {
+	fileToAdd := filepath.Join(util.PathToTestData(), "aptrust-unit-test-job.json")
+	params := url.Values{}
+	params.Add("fullPath", fileToAdd)
+	settings := PostTestSettings{
+		EndpointUrl:              fmt.Sprintf("/validation_jobs/add_file/%s", id),
+		Params:                   params,
+		ExpectedResponseCode:     http.StatusFound,
+		ExpectedRedirectLocation: fmt.Sprintf("/validation_jobs/files/%s?directory=", id),
+		ExpectedContent:          []string{fileToAdd},
+	}
+	DoPostTestWithRedirect(t, settings)
 }
 
 func TestValidationJobDeleteFile(t *testing.T) {
