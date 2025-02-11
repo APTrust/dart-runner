94fd63fe7a70aa5f7b2af221db7346f71fc98dfc
A. Diamond <diamond@sheprador.com>
2025-02-11 15:03:59 -0500

    Display version when started with -version flag

--- dart/main.go
+++ dart/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"flag"
+	"fmt"
+	"os"
 
 	"github.com/APTrust/dart-runner/server"
 )
@@ -11,7 +13,12 @@ var Version string
 
 func main() {
 	port := flag.Int("port", 8444, "Which port should DART listen on?")
+	version := flag.Bool("version", false, "Show version and exit.")
 	flag.Parse()
 	server.SetVersion(Version)
+	if *version {
+		fmt.Println(Version)
+		os.Exit(0)
+	}
 	server.Run(*port, true)
 }
