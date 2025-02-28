44fc5f2d723ac6ae9c33d691758aeee915fe6791
A. Diamond <diamond@sheprador.com>
2025-02-28 10:23:59 -0500

    Open browser window pointing to DART 3 dashboard when app starts.

--- dart/main.go
+++ dart/main.go
@@ -3,8 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
+	"log"
 	"os"
+	"os/exec"
+	"runtime"
+	"time"
 
+	"github.com/APTrust/dart-runner/core"
 	"github.com/APTrust/dart-runner/server"
 )
 
@@ -20,5 +25,38 @@ func main() {
 		fmt.Println(Version)
 		os.Exit(0)
 	}
+	go func() {
+		url := fmt.Sprintf("http://localhost:%d", *port)
+		time.Sleep(1200 * time.Millisecond)
+		openBrowser(url)
+	}()
+
 	server.Run(*port, true)
+	// TODO: Save pid of dart3 process and browser?
+}
+
+func openBrowser(url string) *exec.Cmd {
+	var err error
+	var command *exec.Cmd
+
+	// TODO: See if we can get command PID
+	switch runtime.GOOS {
+	case "linux":
+		command = exec.Command("xdg-open", url)
+	case "windows":
+		command = exec.Command("rundll32", "url.dll,FileProtocolHandler", url)
+	case "darwin":
+		command = exec.Command("open", url)
+	default:
+		err = fmt.Errorf("unsupported platform")
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = command.Start()
+	if err != nil {
+		core.Dart.Log.Errorf("Error launching browser for %s: %v", url, err)
+	}
+	return command
 }
