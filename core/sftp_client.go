e09d7ae2963e24f32484cdfca893a2ca0ff9e633
A. Diamond <diamond@sheprador.com>
2025-10-07 17:16:08 -0400

    Started replacing legacy SFTP client with new client capable of multi-file uploads

--- core/sftp_client.go
+++ core/sftp_client.go
@@ -1,116 +1,166 @@
 package core
 
 import (
-	"context"
 	"fmt"
 	"io"
 	"os"
-	"path"
-	"time"
+	"path/filepath"
 
-	"github.com/machinebox/progress"
-	"github.com/melbahja/goph"
 	"github.com/pkg/sftp"
 	"golang.org/x/crypto/ssh"
 )
 
-// SSHConnect returns an ssh connection or an error. If the StorageService
-// specifies LoginExtra, this will treat that as the path to the SSH key;
-// otherwise, it will use ss.Password as the password. In either case, it
-// uses ss.Login as the user name.
-func SSHConnect(ss *StorageService) (*goph.Client, error) {
-	var auth goph.Auth
-	var err error
+type SFTPClient struct {
+	client *sftp.Client
+}
+
+// GetSFTPAuthMethod returns an authentication method to be used
+// when connecting to the remote server. If ss.LoginExtra is not
+// empty, this returns a ssh.PublicKey AuthMethod using the key
+// found in the file pointed to by ss.LoginExtra. Otherwise, it
+// returns an ssh.Password() AuthMethod using ss.Password.
+func GetSFTPAuthMethod(ss *StorageService) (ssh.AuthMethod, error) {
+	// ss.LoginExtra will be the path to the SSH key file
+	// needed to authenticate this connection.
 	if ss.LoginExtra != "" {
-		auth, err = goph.Key(ss.LoginExtra, "")
-	} else {
-		auth = goph.Password(ss.Password)
+		key, err := os.ReadFile(ss.LoginExtra)
+		if err != nil {
+			return nil, fmt.Errorf("failed to read private key file: %w", err)
+		}
+
+		// Parse private key
+		signer, err := ssh.ParsePrivateKey(key)
+		if err != nil {
+			return nil, fmt.Errorf("failed to parse private key: %w", err)
+		}
+		return ssh.PublicKeys(signer), nil
 	}
+	// If ss.LoginExtra is empty, use password authentication
+	return ssh.Password(ss.Password), nil
+}
+
+// NewSFTPClient creates a new SFTP client connection
+func NewSFTPClient(ss *StorageService) (*SFTPClient, error) {
+	authMethod, err := GetSFTPAuthMethod(ss)
 	if err != nil {
 		return nil, err
 	}
-	config := &goph.Config{
-		Auth:     auth,
-		User:     ss.Login,
-		Addr:     ss.Host,
-		Port:     uint(ss.Port),
-		Timeout:  5 * time.Second,
-		Callback: ssh.InsecureIgnoreHostKey(), // TODO: Change this for production
+	// Configure SSH client
+	config := &ssh.ClientConfig{
+		User: ss.Login,
+		Auth: []ssh.AuthMethod{
+			authMethod,
+		},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(), // Use proper host key checking in production
 	}
-	return goph.NewConn(config)
+
+	// Connect to SSH server
+	addr := fmt.Sprintf("%s:%d", ss.Host, ss.Port)
+	conn, err := ssh.Dial("tcp", addr, config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect to SSH server: %w", err)
+	}
+
+	// Create SFTP client
+	client, err := sftp.NewClient(conn)
+	if err != nil {
+		conn.Close()
+		return nil, fmt.Errorf("failed to create SFTP client: %w", err)
+	}
+
+	return &SFTPClient{client: client}, nil
 }
 
-// SFTPConnect returns an sftp connection or an error. If the StorageService
-// specifies LoginExtra, this will treat that as the path to the SSH key;
-// otherwise, it will use ss.Password as the password. In either case, it
-// uses ss.Login as the user name.
-func SFTPConnect(ss *StorageService) (*sftp.Client, error) {
-	conn, err := SSHConnect(ss)
-	if err != nil {
-		Dart.Log.Errorf("Connection to SFTP service %s at %s failed: %v", ss.Name, ss.HostAndPort(), err)
-		return nil, err
-	}
-	return conn.NewSftp()
+// Close closes the SFTP client connection
+func (sc *SFTPClient) Close() error {
+	return sc.client.Close()
 }
 
-// SFTPUpload uploads a file to the SFTP server described in the
-// StorageService param. localPath is the path to the local file
-// that you want to upload to the remote server. The uploadProgress
-// param should be nil except when running a job from the UI.
-// For jobs launched from the UI, the uploadProgress object will
-// pass progress info back to the front end. Be sure to set
-// uploadProgress.Total to the size of the file and make sure
-// the MessageChannel is initialized.
-func SFTPUpload(ss *StorageService, localPath string, uploadProgress *StreamProgress) (int64, error) {
-	localFile, err := os.Open(localPath)
+// Upload uploads a file or directory from source to destination on the remote server
+func (sc *SFTPClient) Upload(source, destination string) error {
+	// Get file info to determine if source is a file or directory
+	info, err := os.Stat(source)
 	if err != nil {
-		Dart.Log.Errorf("SFTPUpload can't open file %s: %v", localPath, err)
-		return 0, err
-	}
-	client, err := SFTPConnect(ss)
-	if err != nil {
-		return 0, err
+		return fmt.Errorf("failed to stat source: %w", err)
 	}
 
-	remoteFileName := path.Join(ss.Bucket, path.Base(localPath))
+	if info.IsDir() {
+		return sc.uploadDirectory(source, destination)
+	}
+	return sc.uploadFile(source, destination, info)
+}
 
-	err = client.MkdirAll(ss.Bucket)
+// uploadFile uploads a single file to the remote server
+func (sc *SFTPClient) uploadFile(localPath, remotePath string, info os.FileInfo) error {
+	// Open local file
+	srcFile, err := os.Open(localPath)
 	if err != nil {
-		message := fmt.Sprintf("SFTPUpload cannot create remote parent directories for '%s': %v", ss.Bucket, err)
-		Dart.Log.Error(message)
-		return 0, fmt.Errorf(message)
+		return fmt.Errorf("failed to open local file: %w", err)
+	}
+	defer srcFile.Close()
+
+	// Create remote file
+	dstFile, err := sc.client.Create(remotePath)
+	if err != nil {
+		return fmt.Errorf("failed to create remote file: %w", err)
+	}
+	defer dstFile.Close()
+
+	// Copy file contents
+	_, err = io.Copy(dstFile, srcFile)
+	if err != nil {
+		return fmt.Errorf("failed to copy file contents: %w", err)
 	}
 
-	remoteFile, err := client.Create(remoteFileName)
+	// Preserve file permissions
+	err = sc.client.Chmod(remotePath, info.Mode())
 	if err != nil {
-		message := fmt.Sprintf("SFTPUpload cannot create remote file '%s': %v", remoteFileName, err)
-		Dart.Log.Error(message)
-		return 0, fmt.Errorf(message)
+		return fmt.Errorf("failed to set file permissions: %w", err)
 	}
-	defer remoteFile.Close()
 
-	Dart.Log.Infof("SFTPUpload: uploading %s to %s", localPath, remoteFileName)
+	fmt.Printf("Uploaded file: %s -> %s\n", localPath, remotePath)
+	return nil
+}
 
-	bytesWritten := int64(0)
-	if uploadProgress != nil {
-		progressWriter := progress.NewWriter(remoteFile)
-		go func() {
-			progressChan := progress.NewTicker(context.Background(), progressWriter, uploadProgress.Total, 1*time.Second)
-			for p := range progressChan {
-				// TODO: This causes a panic by trying to write to a
-				// closed channel. The JobRunController closes the channel
-				// when the upload is complete, but the progressChan runs
-				// on a ticker that has no idea the messageChannel is closed.
-				//
-				// UPDATE 2023-11-06: Is this still an issue? I haven't seen
-				// it happen in months.
-				uploadProgress.SetTotalBytesCompleted(p.N())
+// uploadDirectory recursively uploads a directory to the remote server
+func (sc *SFTPClient) uploadDirectory(localPath, remotePath string) error {
+	// Walk through the local directory
+	return filepath.Walk(localPath, func(path string, info os.FileInfo, err error) error {
+		if err != nil {
+			return err
+		}
+
+		// Calculate relative path
+		relPath, err := filepath.Rel(localPath, path)
+		if err != nil {
+			return fmt.Errorf("failed to get relative path: %w", err)
+		}
+
+		// Convert to forward slashes for remote path
+		remoteDest := filepath.ToSlash(filepath.Join(remotePath, relPath))
+
+		if info.IsDir() {
+			// Create directory on remote server
+			err = sc.client.MkdirAll(remoteDest)
+			if err != nil {
+				return fmt.Errorf("failed to create remote directory: %w", err)
 			}
-		}()
-		bytesWritten, err = io.Copy(progressWriter, localFile)
-	} else {
-		bytesWritten, err = io.Copy(remoteFile, localFile)
-	}
-	Dart.Log.Infof("SFTPUpload: uploaded %d bytes to %s", bytesWritten, remoteFileName)
-	return bytesWritten, err
+
+			// Preserve directory permissions
+			err = sc.client.Chmod(remoteDest, info.Mode())
+			if err != nil {
+				return fmt.Errorf("failed to set directory permissions: %w", err)
+			}
+
+			fmt.Printf("Created directory: %s\n", remoteDest)
+		} else {
+			// Upload file
+			err = sc.uploadFile(path, remoteDest, info)
+			if err != nil {
+				return err
+			}
+		}
+
+		return nil
+	})
 }
---
+++ core/sftp_client_legacy_test.go
@@ -0,0 +1,135 @@
+package core_test
+
+import (
+	"path/filepath"
+	"sync"
+	"testing"
+
+	"github.com/APTrust/dart-runner/constants"
+	"github.com/APTrust/dart-runner/core"
+	"github.com/APTrust/dart-runner/util"
+	"github.com/google/uuid"
+	"github.com/pkg/sftp"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+const (
+	sftpUserName = "pw_user"
+	sftpPassword = "password"
+	sftpHost     = "127.0.0.1"
+	sftpPort     = 2222
+	tarFileSize  = int64(23552)
+)
+
+// Note: SFTP tests require the SFTP server to be running.
+// scripts/test.rb will start up the server in a docker container.
+func TestSftpUploadWithPassword(t *testing.T) {
+	ss := getSftpStorageService()
+
+	sshClient, err := core.SSHConnect(ss)
+	require.Nil(t, err)
+	require.NotNil(t, sshClient)
+
+	sftpClient, err := core.SFTPConnect(ss)
+	require.Nil(t, err)
+	require.NotNil(t, sftpClient)
+
+	testUploadWithoutProgress(t, sftpClient, ss)
+	testUploadWithProgress(t, sftpClient, ss)
+}
+
+func TestSftpUploadWithSSHKey(t *testing.T) {
+	ss := getSftpStorageService()
+	ss.Login = "key_user"
+	ss.Password = "not-a-valid-password"
+	ss.LoginExtra = filepath.Join(util.PathToTestData(), "sftp", "sftp_user_key")
+
+	sshClient, err := core.SSHConnect(ss)
+	require.Nil(t, err)
+	require.NotNil(t, sshClient)
+
+	sftpClient, err := core.SFTPConnect(ss)
+	require.Nil(t, err)
+	require.NotNil(t, sftpClient)
+
+	testUploadWithoutProgress(t, sftpClient, ss)
+	testUploadWithProgress(t, sftpClient, ss)
+}
+
+func testUploadWithoutProgress(t *testing.T, sftpClient *sftp.Client, ss *core.StorageService) {
+	fileToUpload := filepath.Join(util.PathToTestData(), "bags", "example.edu.sample_good.tar")
+	bytesCopied, err := core.SFTPUpload(ss, fileToUpload, nil)
+	require.Nil(t, err)
+	assert.Equal(t, tarFileSize, bytesCopied)
+}
+
+// This is a tricky test. We want to make sure the SFTP uploader
+// pushes messages into the progress reader's message channel.
+// See comments inline...
+func testUploadWithProgress(t *testing.T, sftpClient *sftp.Client, ss *core.StorageService) {
+	iReadSomething := false
+	messageChannel := make(chan *core.EventMessage)
+
+	// When this function exits, make sure our reader read a message
+	// from the messageChannel. The SFTP uploader is supposed to pump
+	// messages into this channel with info about the upload progress.
+	defer func() {
+		close(messageChannel)
+		assert.True(t, iReadSomething)
+	}()
+
+	// We have to set up a WaitGroup to prevent a data race.
+	// The go routine below is fiddling with the iReadSomething
+	// flag defined above. Unless we wait, our test function could
+	// exit before the go routine below gets to do its work.
+	// That can lead to two problems:
+	// 1. iReadSomething never gets set to true.
+	// 2. The defer function will run too soon, closing the message
+	//    channel, and then the go routine will try to read from a
+	//    closed channel.
+	var wg sync.WaitGroup
+
+	// Define our message channel and a go routine to listen to it.
+	// The go routine will do nothing until later, after we call
+	// core.SFTPUpload below, because until then, there will be no
+	// traffic on the message channel.
+	progress := core.NewStreamProgress(tarFileSize, messageChannel)
+	go func() {
+		if msg, ok := <-messageChannel; ok {
+			assert.Equal(t, "Sent 23.0 kB of 23.0 kB (100%)", msg.Message)
+			iReadSomething = true
+			wg.Done()
+		}
+	}()
+
+	// Tell our WaitGroup we have a task pending.
+	// Then we'll kick off the upload and wait for WaitGroup
+	// to complete. When the go routine above cathches a message
+	// from core.SFTPUpload, it will call wg.Done(). Then the
+	// wg.Wait() at the end of this function will stop waiting.
+	//
+	// At that point, our defer func above will fire, closing
+	// the message channel and testing to see whether the go
+	// routine actually read a message.
+	wg.Add(1)
+	fileToUpload := filepath.Join(util.PathToTestData(), "bags", "example.edu.sample_good.tar")
+	bytesCopied, err := core.SFTPUpload(ss, fileToUpload, progress)
+	require.Nil(t, err)
+	assert.Equal(t, tarFileSize, bytesCopied)
+	wg.Wait()
+}
+
+func getSftpStorageService() *core.StorageService {
+	return &core.StorageService{
+		ID:           uuid.NewString(),
+		Name:         "Local SFTP test service",
+		AllowsUpload: true,
+		Bucket:       "uploads",
+		Host:         sftpHost,
+		Login:        sftpUserName,
+		Password:     sftpPassword,
+		Port:         sftpPort,
+		Protocol:     constants.ProtocolSFTP,
+	}
+}
